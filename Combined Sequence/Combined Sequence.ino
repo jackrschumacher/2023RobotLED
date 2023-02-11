a11e2370854274ef00c9a09611587e72abd27059
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2023-02-11 11:28:47 -0600

    Create Combined Sequences and Add Functions

---
+++ Combined Sequence/Combined Sequence.ino
@@ -0,0 +1,155 @@
+// ArrayOfLedArrays - see https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples for more info on
+// using multiple controllers.  In this example, we're going to set up three NEOPIXEL strips on three
+// different pins, each strip getting its own CRGB array to be played with, only this time they're going
+// to be all parts of an array of arrays.
+// Import library
+#include <FastLED.h>
+
+#define NUM_STRIPS 4
+#define NUM_LEDS_PER_STRIP 81
+CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
+CRGB redStrip[NUM_LEDS_PER_STRIP];
+
+
+// For mirroring strips, all the "special" stuff happens just in setup.  We
+// just addLeds multiple times, once for each strip
+void setup() {
+  FastLED.setBrightness(50);
+  // tell FastLED there's 60 NEOPIXEL leds on pin 2
+  FastLED.addLeds<NEOPIXEL, 12>(leds[0], NUM_LEDS_PER_STRIP);
+  
+
+}
+void REDALLIANCE()
+  {
+    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(255,0,0);
+            // (85,12.7,73.7)
+            FastLED.show();
+        }
+  }
+void CONEREQUEST()
+{
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(230,100,1);
+            // (85,12.7,73.7)
+            FastLED.show();
+            
+            
+        }
+        FastLED.delay(500);
+        CLEAR();
+}
+void CONEAQUIRED()
+  {
+    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(230,100,1);
+            // (85,12.7,73.7)
+            FastLED.show();
+        }
+  }
+void CUBEREQUEST()
+  {
+    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(20,0,25);
+            // (85,12.7,73.7)
+            FastLED.show();
+        }
+        FastLED.delay(500);
+        CLEAR();
+  }
+void CUBEAQUIRED()
+  {
+    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(20,0,25);
+            // (85,12.7,73.7)
+            FastLED.show();
+        }
+  }
+void DISABLED()
+  {
+    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(255,20,0);
+            // (85,12.7,73.7)
+            FastLED.show();
+        }
+  }
+void BLUEALLIANCE()
+  {
+    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(0,0,255);
+            // (85,12.7,73.7)
+            FastLED.show();
+        }
+  }
+void TEAMCOLORS()
+{
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(0,0,255);
+            delay(25);
+            FastLED.show();
+        }
+  FastLED.delay(250);
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            // Does take RGB
+            leds[0][i] = CRGB(255,255,255);
+            delay(25);
+            FastLED.show();
+        }
+  FastLED.delay(250);
+  for(int i = 81; i >= 0; i--) {
+            // Does take RGB
+            leds[0][i] = CRGB(0,0,255);
+            delay(25);
+            FastLED.show();
+        }
+  FastLED.delay(250);
+  for(int i = 81; i >= 0; i--) {
+            // Does take RGB
+            leds[0][i] = CRGB(255,255,255);
+            delay(25);
+            FastLED.show();
+        }
+  FastLED.delay(250);
+}
+void CLEAR(){
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+      // Does take RGB
+      leds[0][i] = CRGB(0,0,0);
+      // (85,12.7,73.7)
+      FastLED.show();
+    }
+}
+
+void loop() {
+    
+    // This inner loop will go over each led in the current strip, one at a time  
+    CONEREQUEST();
+    FastLED.delay(1000);
+    CONEAQUIRED();
+    FastLED.delay(1000);
+    REDALLIANCE();
+    FastLED.delay(1000);
+    CUBEREQUEST();
+    FastLED.delay(1000);
+    CUBEAQUIRED();
+    FastLED.delay(1000);
+    BLUEALLIANCE();
+    FastLED.delay(1000);
+    DISABLED();
+    FastLED.delay(1000);
+    TEAMCOLORS();
+
+    
+      
+      
+  }
+
