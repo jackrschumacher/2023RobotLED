0a6e831596f7d0acaba466a225bb74320226fc5b
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2023-02-11 15:56:18 -0600

    Fix bugs and Update Sequences

--- Combined Sequence/Combined Sequence.ino
+++ Combined Sequence/Combined Sequence.ino
@@ -10,6 +10,8 @@
 CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 CRGB redStrip[NUM_LEDS_PER_STRIP];
 
+#define PIN_ACTIVE  0
+#define PIN_INACTIVE 1
 
 long randNumber1;
 long randNumber2;
@@ -20,8 +22,10 @@ void setup() {
   FastLED.setBrightness(50);
   // tell FastLED there's 60 NEOPIXEL leds on pin 2
   FastLED.addLeds<NEOPIXEL, 12>(leds[0], NUM_LEDS_PER_STRIP);
-  
-
+    pinMode(0,INPUT_PULLUP);
+    pinMode(1,INPUT_PULLUP);
+    pinMode(2,INPUT_PULLUP);
+    pinMode(3,INPUT_PULLUP);
 }
 void REDALLIANCE()
   {
@@ -78,51 +82,51 @@ void DISABLED()
   {
     for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
             // Does take RGB
-            leds[0][i] = CRGB(255,20,0);
+            leds[0][i] = CRGB(235,123,98);
             // (85,12.7,73.7)
             FastLED.show();
         }
   }
 void BLUEALLIANCE()
   {
-    for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
-            leds[0][i] = CRGB(0,0,255);
-            // (85,12.7,73.7)
-            FastLED.show();
-        }
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+          // Does take RGB
+          leds[0][i] = CRGB(0,0,255);
+          // (85,12.7,73.7)
+          FastLED.show();
+      }
   }
-void TEAMCOLORS()
-{
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
-            leds[0][i] = CRGB(0,0,255);
-            delay(25);
-            FastLED.show();
-        }
-  FastLED.delay(250);
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
-            leds[0][i] = CRGB(255,255,255);
-            delay(25);
-            FastLED.show();
-        }
-  FastLED.delay(250);
-  for(int i = 81; i >= 0; i--) {
-            // Does take RGB
-            leds[0][i] = CRGB(0,0,255);
-            delay(25);
-            FastLED.show();
-        }
-  FastLED.delay(250);
-  for(int i = 81; i >= 0; i--) {
-            // Does take RGB
-            leds[0][i] = CRGB(255,255,255);
-            delay(25);
-            FastLED.show();
-        }
-  FastLED.delay(250);
-}
+// void TEAMCOLORS()
+// {
+//   for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+//             // Does take RGB
+//             leds[0][i] = CRGB(0,0,255);
+//             delay(25);
+//             FastLED.show();
+//         }
+//   FastLED.delay(250);
+//   for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+//             // Does take RGB
+//             leds[0][i] = CRGB(255,255,255);
+//             delay(25);
+//             FastLED.show();
+//         }
+//   FastLED.delay(250);
+//   for(int i = 81; i >= 0; i--) {
+//             // Does take RGB
+//             leds[0][i] = CRGB(0,0,255);
+//             delay(25);
+//             FastLED.show();
+//         }
+//   FastLED.delay(250);
+//   for(int i = 81; i >= 0; i--) {
+//             // Does take RGB
+//             leds[0][i] = CRGB(255,255,255);
+//             delay(25);
+//             FastLED.show();
+//         }
+//   FastLED.delay(250);
+// }
 void CLEAR(){
   for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
       // Does take RGB
@@ -143,38 +147,44 @@ void RANDOMLIGHTS(){
         }
 }
 void loop() {
+ 
+  
     int pin0 = digitalRead(0);
     int pin1 = digitalRead(1);
     int pin2 = digitalRead(2);
     int pin3 = digitalRead(3);
-
+    
+   Serial.print(pin0);
+   Serial.print(pin1);
+   Serial.print(pin2);
+   Serial.println(pin3);
     
     // This inner loop will go over each led in the current strip, one at a time
-    if(pin0 == 0 && pin1 == 0 && pin2 == 0 && pin3 == 0){
+    if(pin0 == PIN_INACTIVE && pin1 == PIN_INACTIVE && pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE){
       DISABLED();
-    }
-    else if(pin0 == 1 && pin1 == 1 && pin2 == 1 && pin3 == 1){
-      TEAMCOLORS();
-    }
-    else if(pin0 == 1 && pin1 == 1 && pin2 == 0 && pin3 == 0){
+      }
+    // else if(pin0 == PIN_ACTIVE && pin1 == PIN_ACTIVE && pin2 == PIN_ACTIVE && pin3 == PIN_ACTIVE){
+    //   TEAMCOLORS();
+    // }
+    else if(pin0 == PIN_ACTIVE && pin1 == PIN_ACTIVE && pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE){
       BLUEALLIANCE();
     }
-    else if(pin0 == 1 && pin1 == 1 && pin2 == 1 && pin3 == 0){
+    else if(pin0 == PIN_ACTIVE && pin1 == PIN_ACTIVE && pin2 == PIN_ACTIVE && pin3 == PIN_INACTIVE){
       REDALLIANCE();
     }
-    else if(pin0 == 1 && pin1 == 0 && pin2 == 0 && pin3 == 0){
+    else if(pin0 == PIN_ACTIVE && pin1 == PIN_INACTIVE && pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE){
       CONEREQUEST();
     }
-    else if(pin0 == 0 && pin1 == 0 && pin2 == 0 && pin3 == 1){
+    else if(pin0 == PIN_INACTIVE && pin1 == PIN_INACTIVE && pin2 == PIN_INACTIVE && pin3 == PIN_ACTIVE){
       CONEAQUIRED();
     }
-    else if(pin0 == 0 && pin1 == 1 && pin2 == 0 && pin3 == 0){
+    else if(pin0 == PIN_INACTIVE && pin1 == PIN_ACTIVE && pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE){
       CUBEREQUEST();
     }
-    else if(pin0 == 0 && pin1 == 0 && pin2 == 1 && pin3 == 0){
+    else if(pin0 == PIN_INACTIVE && pin1 == PIN_INACTIVE && pin2 == PIN_ACTIVE && pin3 == PIN_INACTIVE){
       CUBEAQUIRED();
     }
-    else if(pin0 == 0 && pin1 == 0 && pin2 == 1 && pin3 == 1){
+    else if(pin0 == PIN_INACTIVE && pin1 == PIN_INACTIVE && pin2 == PIN_ACTIVE && pin3 == PIN_ACTIVE){
       RANDOMLIGHTS();      
     }
     else{
