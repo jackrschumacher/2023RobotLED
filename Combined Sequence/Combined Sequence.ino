7e9659483fdc65a9d439a2e510d2416f9ead88af
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2023-02-17 19:27:33 -0600

    Add Comments
    Added comments for documentation. Removed some old comments to improve readability

--- Combined Sequence/Combined Sequence.ino
+++ Combined Sequence/Combined Sequence.ino
@@ -1,8 +1,4 @@
-// ArrayOfLedArrays - see https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples for more info on
-// using multiple controllers.  In this example, we're going to set up three NEOPIXEL strips on three
-// different pins, each strip getting its own CRGB array to be played with, only this time they're going
-// to be all parts of an array of arrays.
-// Import library
+// Create 4 array's of LED's with 81 LEDs each
 #include <FastLED.h>
 
 #define NUM_STRIPS 4
@@ -10,14 +6,16 @@
 CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
 CRGB redStrip[NUM_LEDS_PER_STRIP];
 
+// Define active state of pins vs inactive pins
 #define PIN_ACTIVE  0
 #define PIN_INACTIVE 1
 
+// Intialize random numbers for random LED pattern
 long randNumber1;
 long randNumber2;
 long randNumber3;
-// For mirroring strips, all the "special" stuff happens just in setup.  We
-// just addLeds multiple times, once for each strip
+
+// Setup the different strips of LEDs 
 void setup() {
   FastLED.setBrightness(50);
   // tell FastLED there's 60 NEOPIXEL leds on pin 2
@@ -27,21 +25,18 @@ void setup() {
     pinMode(2,INPUT_PULLUP);
     pinMode(3,INPUT_PULLUP);
 }
+// Different LED patterns
 void REDALLIANCE()
   {
     for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
             leds[0][i] = CRGB(255,0,0);
-            // (85,12.7,73.7)
             FastLED.show();
         }
   }
 void CONEREQUEST()
 {
   for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
             leds[0][i] = CRGB(230,100,1);
-            // (85,12.7,73.7)
             FastLED.show();
             
             
@@ -52,18 +47,14 @@ void CONEREQUEST()
 void CONEAQUIRED()
   {
     for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
             leds[0][i] = CRGB(230,100,1);
-            // (85,12.7,73.7)
             FastLED.show();
         }
   }
 void CUBEREQUEST()
   {
     for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
             leds[0][i] = CRGB(20,0,25);
-            // (85,12.7,73.7)
             FastLED.show();
         }
         FastLED.delay(500);
@@ -72,72 +63,60 @@ void CUBEREQUEST()
 void CUBEAQUIRED()
   {
     for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
             leds[0][i] = CRGB(20,0,25);
-            // (85,12.7,73.7)
             FastLED.show();
         }
   }
 void DISABLED()
   {
     for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
             leds[0][i] = CRGB(235,123,98);
-            // (85,12.7,73.7)
             FastLED.show();
         }
   }
 void BLUEALLIANCE()
   {
-  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-          // Does take RGB
-          leds[0][i] = CRGB(0,0,255);
-          // (85,12.7,73.7)
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) 
+          leds[0][i] = CRGB(0,0,255)
           FastLED.show();
       }
   }
-// void TEAMCOLORS()
-// {
-//   for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-//             // Does take RGB
-//             leds[0][i] = CRGB(0,0,255);
-//             delay(25);
-//             FastLED.show();
-//         }
-//   FastLED.delay(250);
-//   for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-//             // Does take RGB
-//             leds[0][i] = CRGB(255,255,255);
-//             delay(25);
-//             FastLED.show();
-//         }
-//   FastLED.delay(250);
-//   for(int i = 81; i >= 0; i--) {
-//             // Does take RGB
-//             leds[0][i] = CRGB(0,0,255);
-//             delay(25);
-//             FastLED.show();
-//         }
-//   FastLED.delay(250);
-//   for(int i = 81; i >= 0; i--) {
-//             // Does take RGB
-//             leds[0][i] = CRGB(255,255,255);
-//             delay(25);
-//             FastLED.show();
-//         }
-//   FastLED.delay(250);
-// }
-void CLEAR(){
+void TEAMCOLORS()
+{
   for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-      // Does take RGB
-      leds[0][i] = CRGB(0,0,0);
-      // (85,12.7,73.7)
+            leds[0][i] = CRGB(0,0,255);
+            delay(25);
+            FastLED.show();
+        }
+  FastLED.delay(250);
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+            leds[0][i] = CRGB(255,255,255);
+            delay(25);
+            FastLED.show();
+        }
+  FastLED.delay(250);
+  for(int i = 81; i >= 0; i--) {
+            leds[0][i] = CRGB(0,0,255);
+            delay(25);
+            FastLED.show();
+        }
+  FastLED.delay(250);
+  for(int i = 81; i >= 0; i--) {
+            leds[0][i] = CRGB(255,255,255);
+            delay(25);
+            FastLED.show();
+        }
+  FastLED.delay(250);
+}
+void CLEAR(){
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++
+      leds[0][i] = CRGB(0,0,
       FastLED.show();
     }
 }
 void RANDOMLIGHTS(){
   for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-            // Does take RGB
+            
             randNumber1 = random(0, 255);
             randNumber2 = random(0,225);
             randNumber3 = random(0,225);
@@ -146,26 +125,27 @@ void RANDOMLIGHTS(){
             FastLED.show();
         }
 }
-void loop() {
+void AUTOBALANCE(){
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+                     
+}
+void loop(){
  
+  // Initialize digital Read pins
+  int pin0 = digitalRead(0);
+  int pin1 = digitalRead(1);
+  int pin2 = digitalRead(2);
+  int pin3 = digitalRead(3);
   
-    int pin0 = digitalRead(0);
-    int pin1 = digitalRead(1);
-    int pin2 = digitalRead(2);
-    int pin3 = digitalRead(3);
+   
     
-   Serial.print(pin0);
-   Serial.print(pin1);
-   Serial.print(pin2);
-   Serial.println(pin3);
-    
-    // This inner loop will go over each led in the current strip, one at a time
+    // If statements determining which pattern is being sent from the RIO w/ DIO
     if(pin0 == PIN_INACTIVE && pin1 == PIN_INACTIVE && pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE){
       DISABLED();
-      }
-    // else if(pin0 == PIN_ACTIVE && pin1 == PIN_ACTIVE && pin2 == PIN_ACTIVE && pin3 == PIN_ACTIVE){
-    //   TEAMCOLORS();
-    // }
+    }
+    else if(pin0 == PIN_ACTIVE && pin1 == PIN_ACTIVE && pin2 == PIN_ACTIVE && pin3 == PIN_ACTIVE){
+      TEAMCOLORS();
+    }
     else if(pin0 == PIN_ACTIVE && pin1 == PIN_ACTIVE && pin2 == PIN_INACTIVE && pin3 == PIN_INACTIVE){
       BLUEALLIANCE();
     }
@@ -190,13 +170,9 @@ void loop() {
     else{
       CLEAR();
     }
-    // else if(pin0 == 1 && pin1 == 0 && pin2 == 1 && pin3 == 0){
-    //   AUTOBALANCE();
-    // }
-   
+    else if(pin0 == 1 && pin1 == 0 && pin2 == 1 && pin3 == 0){
+      AUTOBALANCE();
+    }
 
-    
-      
-      
-  }
+}
 
